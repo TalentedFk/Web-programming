c023c404db6f90a3d6b6e12dfb5a14d68b3396a9
TalentedFk <bouman9617@gmail.com>
2023-07-11 19:41:35 +0100

    web programming

---
+++ hello.java
@@ -0,0 +1,17 @@
+public Class Hello{
+	public static void main(String[] args) {
+		int x, y, z;
+		Scanner input = new Scanner(System.in);
+		System.out.printin("Enter The first Integer");
+		x = input.nextInt();
+		System.out.printin("Enter The second Integer");
+		y = input.nextInt();
+		System.out.printin("Enter The third Integer");
+		z = input.nextInt();
+		int sum = x + y + z;
+		System.out.printin("The sum is: + sum");
+
+
+		
+	}
+}
